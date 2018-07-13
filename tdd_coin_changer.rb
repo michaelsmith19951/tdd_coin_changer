478b887dab5e47b8d79be676138e0c1d58679c33
Michael Smith <michaelsmith19951@yahoo.com>
2018-07-13 12:56:28 -0400

    2nd test that failed, test_if_hash_is_class

--- tdd_coin_changer.rb
+++ tdd_coin_changer.rb
@@ -1,7 +1,35 @@
 
-coins = {
-	'quarters' => 25,
-	'dimes' => 10,
-	'nickels' => 5,
-	'pennies' => 1
-}
+# coins = {
+# 	'quarters' => 25,
+# 	'dimes' => 10,
+# 	'nickels' => 5,
+# 	'pennies' => 1
+# }
+
+def coin_changer(amount)
+	coins = []
+	# while amount >= 25 do
+	# 	coins[:quarter] += 1
+	# 	amount -= 25
+	# end
+
+
+	# while amount >= 10 do
+	# 	coins[:dime] += 1
+	# 	amount -= 10
+	# end
+
+	# while amount >= 5 do
+	# 	coins[:nickel] += 1
+	# 	amount -= 5
+	# end
+
+	# while amount >= 1 do
+	# 	coins[:penny] += 1
+	# 	amount -= 1
+	# end
+
+coins
+
+
+end
--- test_tdd_coin_changer.rb
+++ test_tdd_coin_changer.rb
@@ -7,7 +7,8 @@ require_relative "tdd_coin_changer.rb"
 # Test the test_new_calc.rb for passes and failures
 class TestTddCoinChanger < Minitest::Test
 
-	def test_assert_that_quarters_is_25
-		assert_equal(25, "quarters")
+	def test_if_hash_is_class
+		amount = 1
+		assert_equal(Hash, coin_changer(amount).class)
 	end
 end
