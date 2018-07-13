8043e86512723c4c7f0e246005591c7dfecdd1dd
Michael Smith <michaelsmith19951@yahoo.com>
2018-07-13 13:05:28 -0400

    5th test that failed, test_if_dime_equals_10

--- tdd_coin_changer.rb
+++ tdd_coin_changer.rb
@@ -10,14 +10,14 @@ def coin_changer(amount)
 	coins = {:quarter => 0, :dime => 0, :nickel => 0, :penny => 0}
 	while amount >= 25 do
 		coins[:quarter] += 1
-		amount -= 15
+		amount -= 25
 	end
 
 
-	# while amount >= 10 do
-	# 	coins[:dime] += 1
-	# 	amount -= 10
-	# end
+	while amount >= 10 do
+		coins[:dime] += 1
+		amount -= 10
+	end
 
 	# while amount >= 5 do
 	# 	coins[:nickel] += 1
--- test_tdd_coin_changer.rb
+++ test_tdd_coin_changer.rb
@@ -12,8 +12,13 @@ class TestTddCoinChanger < Minitest::Test
 		assert_equal(Hash, coin_changer(amount).class)
 	end
 
-	def test_if_quarter_equals_25
-		amount = 25
-		assert_equal({:quarter => 1}, coin_changer(amount))
+	# def test_if_quarter_equals_25
+	# 	amount = 25
+	# 	assert_equal({:quarter => 1}, coin_changer(amount))
+	# end
+
+	def test_if_dime_equals_10
+		amount = 10
+		assert_equal({:dime => 1}, coin_changer(amount))
 	end
 end
