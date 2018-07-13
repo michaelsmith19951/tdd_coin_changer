7c5c0ca4e2d9d1a13141fc93bb67a8f85bf5af77
Michael Smith <michaelsmith19951@yahoo.com>
2018-07-13 13:24:28 -0400

    11th test that passed, test_if_dime_equals_10

--- test_tdd_coin_changer.rb
+++ test_tdd_coin_changer.rb
@@ -17,10 +17,10 @@ class TestTddCoinChanger < Minitest::Test
 		assert_equal({:quarter => 1, :dime => 0, :nickel => 0, :penny => 0}, coin_changer(amount))
 	end
 
-	# def test_if_dime_equals_10
-	# 	amount = 10
-	# 	assert_equal({:dime => 1}, coin_changer(amount))
-	# end
+	def test_if_dime_equals_10
+		amount = 10
+		assert_equal({:quarter => 0, :dime => 1, :nickel => 0, :penny => 0}, coin_changer(amount))
+	end
 
 	# def test_if_nickel_equals_5
 	# 	amount = 5
